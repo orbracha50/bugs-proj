4b3f2fcab20d73da46cbde3fcd5746e33894b5fe
Or Bracha <orbracha50@gmail.com>
2024-07-03 13:41:42 +0300

    Add login & signup to the app header

--- public/cmps/AppHeader.jsx
+++ public/cmps/AppHeader.jsx
@@ -1,12 +1,29 @@
-const { NavLink } = ReactRouterDOM
-const { useEffect } = React
+const { useState } = React
+
+const { Link, NavLink } = ReactRouterDOM
+const { useNavigate } = ReactRouter
+
+import { userService } from '../services/user.service.js' 
+import { showErrorMsg } from '../services/event-bus.service.js'
 
 import { UserMsg } from './UserMsg.jsx'
+import { LoginSignup } from './LoginSignUp.jsx'
 
 export function AppHeader() {
-    useEffect(() => {
-        // component did mount when dependancy array is empty
-    }, [])
+	const navigate = useNavigate()
+	const [user, setUser] = useState(userService.getLoggedinUser())
+
+	function onLogout() {
+		userService.logout()
+            .then(() => onSetUser(null))
+            .catch(err => showErrorMsg('OOPs try again'))
+	}
+
+	function onSetUser(user) {
+		setUser(user)
+		navigate('/')
+	}
+
 
     return (
         <header className='container'>
@@ -15,6 +32,16 @@ export function AppHeader() {
                 <NavLink to="/">Home</NavLink> |<NavLink to="/bug">Bugs</NavLink> |
                 <NavLink to="/about">About</NavLink>
             </nav>
+            {user ? (
+				<section>
+					<Link to={`/user/${user._id}`}>Hello {user.fullname}</Link>
+					<button onClick={onLogout}>Logout</button>
+				</section>
+			) : (
+				<section>
+					<LoginSignup onSetUser={onSetUser} />
+				</section>
+			)}
             <h1>Bugs are Forever</h1>
         </header>
     )
